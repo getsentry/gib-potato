0c5ede9bb32f729f947d2bfc0a044d4e44d9b9fa
Krystof Woldrich <krystofwoldrich@gmail.com>
2022-08-22 13:45:26 +0200

    Add slack login view

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,9 +6,23 @@ import router from './router'
 
 import './assets/main.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add some free styles */
+import { faSlack } from '@fortawesome/free-brands-svg-icons'
+
+/* add each imported icon to the library */
+library.add(faSlack)
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
