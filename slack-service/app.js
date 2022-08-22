fb269ba2fd3684e0bcac46770856df806580a0b6
tobias-wilfert <tobias.wilfert@sentry.io>
2022-08-22 14:39:13 +0200

    Added some todos

--- slack-service/app.js
+++ slack-service/app.js
@@ -1,27 +1,39 @@
 const { App } = require('@slack/bolt');
 
+// TODO: Make a connection with the DB via an ORM??
+// TODO: Find a way to make this work with an env file
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-// Listens to incoming messages that contain "hello"
-app.message(':potato:', async ({ message, say }) => {
-  const text = message.text
-  const senderId = message.user
-  let senderName = ''
-
+async function getUserNameById(userId) {
   try {
     // Call the users.info method using the WebClient
     const result = await app.client.users.info({
       user: senderId
     });
-    senderName = result["real_name"]
+    return result["real_name"]
   }
   catch (error) {
-    console.error(error);
+    console.error(error); // Maybe do some proper error handling here
+    return "";
   }
+}
 
+// Listens to incoming messages that contain "hello"
+app.message(':potato:', async ({ message, say }) => {
+  const text = message.text
+  // Find the user in the BD with the Slack UID
+  let senderName = await getUserNameById(message.user) // <- How do we find this person in the DB ????
+  // TODO: So need to use the UID from slack for our ID, ask @michi
+
+  // TODO: Grab the users mentioned in the text
+  // TODO: Check if it is a valid amount
+  // TODO: Check the number of tacos the sender has given
+  // TODO: Push the changes to the DB
+
+  // We probably don't want to send a message later on
   // say() sends a message to the channel where the event was triggered
   await say(`Hey there <@${message.user}>!`);
 });
