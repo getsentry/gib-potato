06698f251b8888fa4617a8c42050aba91ea2e126
Michael Hoffmann <michael.hoffmann@sentry.io>
2022-08-22 15:56:21 +0200

    Migrations

--- docker/web/run.sh
+++ docker/web/run.sh
@@ -3,6 +3,7 @@
 service apache2 start
 service php8.1-fpm start
 
-cd /var/www/gib-potato && bin/cake migrations migrate
+bin/cake migrations migrate
 
-exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+# Start supervisord and services
+exec /usr/bin/supervisord  -n -c /etc/supervisor/supervisord.conf
