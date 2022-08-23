142bd894c43c8bd3ab1173696d3c30472aaad559
tobias-wilfert <tobias.wilfert@sentry.io>
2022-08-23 09:44:08 +0200

    PoC ORM

---
+++ slack-service/ReadMe.md
@@ -0,0 +1,1 @@
+**Note** the link in the `schema.prisma` file needs to be changed when this is later run inside docker.
--- slack-service/app.js
+++ slack-service/app.js
@@ -1,13 +1,55 @@
+// ORM
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+// Secrets for Slack API
 require('dotenv').config()
+// Slack API
 const { App } = require('@slack/bolt');
 
-// TODO: Make a connection with the DB via an ORM??
-// TODO: Find a way to make this work with an env file
+
+// PoC to get info from the DB
+async function query() {
+  const allUsers = await prisma.users.findMany()
+  console.log(allUsers)
+}
+
+// PoC to get info into the DB
+/// Find the user in the DB that has a matching slack ID, if there is none return null
+async function getUserBySlackId(slackId){
+  const user  = await prisma.users.findFirst({
+    where: {
+      slack_uid: slackId
+    }
+  })
+
+  return user
+}
+
+// Returns the UTC time in the MySql DateTime format
+function getDateTime(){
+  return new Date().toISOString().slice(0, 19).replace('T', ' ')
+}
+
+/// Adds a user to the Database
+async function addUser(fullName, slackId){
+  await prisma.users.create({
+    data : {
+      id : "1", // TODO: Check if we can fill in this using the ORM, also ask if a serial might not be nicer here
+      slack_full_name : fullName,
+      slack_uid : slackId,
+      created :  new Date(),
+      modified :  new Date()
+    }
+  })
+}
+
+// Initialize the slack App
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+// Get the full name using the userId from the Slack API
 async function getUserNameById(userId) {
   try {
     // Call the users.info method using the WebClient
@@ -25,31 +67,48 @@ async function getUserNameById(userId) {
 // Listens to incoming messages that contain "hello"
 app.message(':potato:', async ({ message, say }) => {
   const text = message.text
-  // Find the user in the BD with the Slack UID
+  
   let senderName = await getUserNameById(message.user) // <- How do we find this person in the DB ????
-
+  // Find the user in the BD with the Slack UID
+  let sender = await getUserBySlackId(message.user)
+  if(!sender){
+    console.log("Seems like the user is not yet in the DB")
+    // TODO: Need to add the User to the DB
+  }
+  
   const regex = /<.*?>/g; // Regex to find all the mentions
-  let usersFound  = text.match(regex);
-  usersFound = usersFound.filter((t) => t !== `<@${message.user}>` ) // Remove the sender if he is in the message
+  let usersFound = text.match(regex);
+
+  if(!usersFound || usersFound.length == 0){ // Check needed for the filter
+    await say("Seems like no one was tagged in that message");
+    return
+  }
+
+  usersFound = usersFound.filter((t) => t !== `<@${message.user}>`) // Remove the sender if he is in the message
+
+  if(!usersFound || usersFound.length == 0){ // Check needed for the length
+    await say("Seems like no one was tagged in that message"); // <- Think about if we maybe want to handle this differently
+    return
+  }
 
   let userCount = usersFound.length
   let potatoCount = (text.match(/:potato:/g) || []).length
 
   // Check the total amount doesn't surpass the 5 limit
-  if(userCount*potatoCount > 5){
+  if (userCount * potatoCount > 5) {
     await say("You don't have that much potato's");
     return
   }
   // TODO: Check that the number of potato's given is smaller or equal to the amount the user has
 
-  // This is just to check that we can find all the people
-  let responds =  "The following people got a potato:"
+  // This is just to check that we can find all the people ->  Seems to work
+  let responds = "The following people got a potato:"
   usersFound.forEach(user => {
     responds += user
   });
 
 
-  // TODO: Check the number of tacos the sender has given
+  // TODO: Check the number of potato's the sender has given
   // TODO: Push the changes to the DB
 
   // We probably don't want to send a message later on
@@ -61,5 +120,17 @@ app.message(':potato:', async ({ message, say }) => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
+  /* The proper way to work with the DB
+  query().then(async () => {
+    await prisma.$disconnect() // Do not want to have this in later versions since we need multiple queries
+  })
+    .catch(async (e) => { // Need to see if we want to be that strict about things
+      console.error(e)
+      await prisma.$disconnect()
+      process.exit(1)
+    })
+  */
+
   console.log('⚡️ Bolt app is running!');
+  console.log(getDateTime())
 })();
