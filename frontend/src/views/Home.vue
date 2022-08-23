4235e947794b8979f1f65584f2b0ac7918773a66
Krystof Woldrich <krystofwoldrich@gmail.com>
2022-08-23 09:08:54 +0200

    Remove extras from mobile menu

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -126,7 +126,7 @@ const userNavigation = [
                 <div class="pt-3 pb-2">
                   <div class="flex items-center justify-between px-4">
                     <div>
-                      <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="Workflow" />
+                      <Logo class="h-8 w-auto" />
                     </div>
                     <div class="-mr-2">
                       <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -137,10 +137,6 @@ const userNavigation = [
                   </div>
                   <div class="mt-3 px-2 space-y-1">
                     <a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">Home</a>
-                    <a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">Profile</a>
-                    <a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">Resources</a>
-                    <a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">Company Directory</a>
-                    <a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">Openings</a>
                   </div>
                 </div>
                 <div class="pt-4 pb-2">
@@ -150,12 +146,7 @@ const userNavigation = [
                     </div>
                     <div class="ml-3 min-w-0 flex-1">
                       <div class="text-base font-medium text-gray-800 truncate">{{ user.name }}</div>
-                      <div class="text-sm font-medium text-gray-500 truncate">{{ user.email }}</div>
                     </div>
-                    <button type="button" class="ml-auto flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      <span class="sr-only">View notifications</span>
-                      <BellIcon class="h-6 w-6" aria-hidden="true" />
-                    </button>
                   </div>
                   <div class="mt-3 px-2 space-y-1">
                     <a v-for="item in userNavigation" :key="item.name" :href="item.href" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-100 hover:text-gray-800">{{ item.name }}</a>
