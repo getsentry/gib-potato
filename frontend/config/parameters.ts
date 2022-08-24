335350564c8f82eca93abcb58a574f7c0b645d0a
Krystof Woldrich <krystofwoldrich@gmail.com>
2022-08-24 16:13:13 +0200

    Add slack auth url builder

--- frontend/config/parameters.ts
+++ frontend/config/parameters.ts
@@ -1,6 +1,14 @@
 
-export default {
+const params = {
   api: {
     host: import.meta.env.VITE_APP_API_HOST,
   },
-}
+  slack: {
+    clientId: import.meta.env.VITE_APP_SLACK_CLIENT_ID,
+    redirectUri: import.meta.env.VITE_APP_SLACK_REDIRECT_URI,
+    baseUrl: 'https://slack.com/openid/connect/authorize',
+    scopes: ['openid', 'email', 'profile'],
+  },
+} as const;
+
+export default params;
