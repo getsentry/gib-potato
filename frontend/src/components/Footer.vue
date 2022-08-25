7804e6b212aeea59a9bc6796641549c9b755510b
Michael Hoffmann <michael.hoffmann@sentry.io>
2022-08-25 02:57:55 +0200

    Copy

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -3,9 +3,9 @@
     <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
       <div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
         <span class="block sm:inline">
-          &copy; 2022 Sentry Hackweek.
+          &copy; 2022 Sentry.
         </span>
-        <span class="block sm:inline">Enjoy.</span>
+        <span class="block sm:inline">by Tobi, Gino, Krys and Michi</span>
       </div>
     </div>
   </footer>
