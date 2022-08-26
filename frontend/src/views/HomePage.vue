2616c8144029da96c4d9805e45f2d3542562c20f
Michael Hoffmann <michael.hoffmann@sentry.io>
2022-08-26 18:32:54 +0200

    No more linting issues

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
 import UsersLeaderboard from '../components/UsersLeaderboard.vue';
-import router from '../router';
 import { useAccountStore } from '../stores/account';
-import { storeToRefs } from 'pinia';
 
 const accountStore = useAccountStore();
-const { account } = storeToRefs(accountStore);
-
-const navigation = [{ name: 'Home', href: '/', current: true }];
 const logout = () => accountStore.logout();
 </script>
 
@@ -15,7 +10,9 @@ const logout = () => accountStore.logout();
   <div>
     <UsersLeaderboard />
     <footer class="p-2 flex justify-center">
-      <span class="text-sm"> &copy; {{ new Date().getFullYear() }} Sentry </span>
+      <span class="text-sm">
+        &copy; {{ new Date().getFullYear() }} Sentry
+      </span>
       <span class="mx-2"> &middot; </span>
       <a
         class="text-sm cursor-pointer"
