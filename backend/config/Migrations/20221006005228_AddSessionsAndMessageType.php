e61d5983f1a81812bfc34e86abd9528e38016591
Michael Hoffmann <michael.hoffmann@sentry.io>
2022-10-06 02:52:58 +0200

    Add new table and message type field

---
+++ backend/config/Migrations/20221006005228_AddSessionsAndMessageType.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AddSessionsAndMessageType extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up()
+    {
+        $this->table('sessions', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'char', [
+                'default' => null,
+                'limit' => 40,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('data', 'binary', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('expires', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->create();
+
+        $this->table('messages')
+            ->addColumn('type', 'string', [
+                'after' => 'amount',
+                'default' => null,
+                'length' => 255,
+                'null' => false,
+            ])
+            ->update();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down()
+    {
+
+        $this->table('messages')
+            ->removeColumn('type')
+            ->update();
+
+        $this->table('sessions')->drop()->save();
+    }
+}
