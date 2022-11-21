0a3d7e8cc86eac8bf444dc246dd9a5b8fe946cf4
Luca Forstner <luca.forstner@sentry.io>
2022-11-21 14:23:32 +0100

    build(frontend): Add `@sentry/vite-plugin`

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,27 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import sentryVitePlugin from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    sentryVitePlugin({
+      include: 'dist',
+      org: 'sentry',
+      project: 'gibpotato-frontend',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      dryRun: !process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   envDir: 'config',
+  build: {
+    sourcemap: true,
+  },
 });
