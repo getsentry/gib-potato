e575030d317870fdd8a9fbf730c36641443f495f
Michael Hoffmann <michael.hoffmann@sentry.io>
2022-11-21 23:25:04 +0100

    Port sentry unpluggin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,40 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import sentryVitePlugin from '@sentry/vite-plugin';
 import path from 'path';
+
+if (!process.env.SENTRY_AUTH_TOKEN) {
+  console.log(
+    'SENTRY_AUTH_TOKEN not set. Will not upload GibPotato release artifacts to Sentry.'
+  );
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+  vue(),
+  sentryVitePlugin({
+    include: 'dist',
+    org: 'sentry',
+    project: 'gibpotato-frontend',
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    dryRun: !process.env.SENTRY_AUTH_TOKEN,
+    cleanArtifacts: true,
+    setCommits: {
+      auto: true,
+      ignoreEmpty: true,
+    },
+  }),
+  ],
   build: {
-      emptyOutDir: false,
-      outDir: './webroot/',
-      manifest: true,
-      minify: 'esbuild',
-      rollupOptions: {
-          input: './frontend/src/main.ts'
-      },
+    sourcemap: true,
+    emptyOutDir: false,
+    outDir: './webroot/',
+    manifest: true,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: './frontend/src/main.ts'
+    },
   },
   resolve: {
     alias: {
