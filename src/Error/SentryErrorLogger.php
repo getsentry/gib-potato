02d9efb6ccbb4a7350d8465ba2bc84c8f6959680
Michael Hoffmann <michael.hoffmann@sentry.io>
2022-12-01 00:49:42 +0100

    CS

--- src/Error/SentryErrorLogger.php
+++ src/Error/SentryErrorLogger.php
@@ -9,6 +9,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Sentry\Event;
 use Sentry\EventHint;
 use Sentry\ExceptionMechanism;
+use Sentry\Severity;
 use Throwable;
 use function Sentry\captureEvent;
 use function Sentry\captureMessage;
@@ -23,7 +24,7 @@ class SentryErrorLogger extends ErrorLogger
      */
     public function logError(PhpError $error, ?ServerRequestInterface $request = null, bool $includeTrace = false): void
     {
-        captureMessage($error->getMessage(), \Sentry\Severity::fromError($error->getCode()));
+        captureMessage($error->getMessage(), Severity::fromError($error->getCode()));
 
         parent::logError($error, $request, $includeTrace);
     }
