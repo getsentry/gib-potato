50b78bfea46575161f87ce140add4eaf19ab3ffb
Michael Hoffmann <michael.hoffmann@sentry.io>
2022-12-20 05:22:15 +0100

    Add potal

---
+++ potal/go.mod
@@ -0,0 +1,10 @@
+module github.com/getsentry/gib-potato/potal
+
+go 1.19
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/slack-go/slack v0.11.4
+)
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ potal/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+)
+
+// Global slackClient, initialized in main()
+var slackClient *slack.Client
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	slackClient = slack.New(os.Getenv("SLACK_BOT_TOKEN"), slack.OptionDebug(true))
+
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
+		sv, err := slack.NewSecretsVerifier(r.Header, signingSecret)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if _, err := sv.Write(body); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err := sv.Ensure(); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Printf("Event received: %s \n", eventsAPIEvent.Type)
+
+		if eventsAPIEvent.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal([]byte(body), &r)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "text")
+			w.Write([]byte(r.Challenge))
+		}
+
+	})
+	fmt.Println("🥔 GibPotato app is running!")
+	http.ListenAndServe(":3000", nil)
+}
