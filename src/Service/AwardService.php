b236e92783befbf85ea4ce99bc2421301547d0f1
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-01-17 00:50:49 +0100

    Rework AwardService

--- src/Service/AwardService.php
+++ src/Service/AwardService.php
@@ -5,8 +5,9 @@ namespace App\Service;
 
 use App\Model\Entity\User;
 use App\Model\Table\MessagesTable;
+use App\Service\Event\MessageEvent;
+use App\Service\Event\ReactionAddedEvent;
 use Cake\ORM\Locator\LocatorAwareTrait;
-use Cake\I18n\FrozenTime;
 
 class AwardService
 {
@@ -21,26 +22,40 @@ class AwardService
         $this->Messages = $this->fetchTable('Messages');
     }
 
-    public function gib(User $fromUser, User $toUser, int $amount, string $type)
-    {
+    public function gib(
+        User $fromUser,
+        User $toUser,
+        MessageEvent|ReactionAddedEvent $event,
+    ) {
         $message = $this->Messages->newEntity([
             'sender_user_id' => $fromUser->id,
             'receiver_user_id' => $toUser->id,
-            'amount' => $amount,
-            'type' => $type,
+            'amount' => $event->amount,
+            'type' => str_replace(':', '', $event->reaction),
         ]);
         $this->Messages->saveOrFail($message);
 
-        // @FIXME Allow users to opt-out
-        $this->slackClient->postMessage(
-            channel: $fromUser->slack_user_id,
-            text: sprintf('You did gib *%s* :%s: to <@%s>.', $amount, $type, $toUser->slack_user_id),
-        );
+        if ($fromUser->notifications['sent'] === true) {
+            $gibMessage = sprintf('You did gib *%s* %s to <@%s>.', $event->amount, $event->reaction, $toUser->slack_user_id);
+            $gibMessage .= PHP_EOL;
+            $gibMessage .= sprintf('> %s', $event->permalink);
 
-        // @FIXME Allow users to opt-out
-        $this->slackClient->postMessage(
-            channel: $toUser->slack_user_id,
-            text: sprintf('<@%s> did gib you *%s* :%s:.', $fromUser->slack_user_id, $amount, $type),
-        );
+            $this->slackClient->postMessage(
+                channel: $fromUser->slack_user_id,
+                text: $gibMessage,
+            );
+        }
+
+        if ($toUser->notifications['received'] === true) {
+            $receivedMessage = sprintf('<@%s> did gib you *%s* %s.', $fromUser->slack_user_id, $event->amount, $event->reaction);
+            $receivedMessage .= PHP_EOL;
+            $receivedMessage .= sprintf('> %s', $event->permalink);
+
+            // @FIXME Allow users to opt-out
+            $this->slackClient->postMessage(
+                channel: $toUser->slack_user_id,
+                text: $receivedMessage,
+            );
+        }
     }
 }
