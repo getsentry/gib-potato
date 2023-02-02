a7c6e916c16ec6bff0c7b13be4ccb32987678547
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-02 22:17:32 +0100

    Login styling

--- templates/Login/login.php
+++ templates/Login/login.php
@@ -10,14 +10,14 @@
         <?= $this->Html->image('logo.png', ['class' => 'w-32 mb-4 mx-auto rounded-full']); ?>
 
         <h1 class="text-center text-3xl font-bold mb-16">
-            Gib Potato
+            GibPotato
         </h1>
 
         <?= $this->Html->link('Sign in with Slack', [
                 'controller' => 'Login',
                 'action' => 'startOpenId',
             ], [
-                'class' => 'inline-flex items-center px-6 py-3 border border-transparent text-base font-semibold rounded-md text-amber-700 bg-amber-100 hover:bg-amber-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500 focus:ring-offset-gray-50 dark:focus:ring-offset-zinc-900',
+                'class' => 'inline-flex items-center px-6 py-3 border border-transparent text-base font-semibold rounded-md text-zinc-900 bg-amber-200',
             ]);
         ?>
     </div>
