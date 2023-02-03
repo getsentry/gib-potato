997a17cd8d4aa0bca48876d5eeac505bf4c6b8e2
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-03 03:32:34 +0100

    Use User methods

--- src/Service/AwardService.php
+++ src/Service/AwardService.php
@@ -3,13 +3,10 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-use App\Model\Entity\Message;
 use App\Model\Entity\User;
 use App\Model\Table\MessagesTable;
-use App\Model\Table\UsersTable;
 use App\Service\Event\MessageEvent;
 use App\Service\Event\ReactionAddedEvent;
-use Cake\I18n\FrozenTime;
 use Cake\ORM\Locator\LocatorAwareTrait;
 
 class AwardService
@@ -17,12 +14,10 @@ class AwardService
     use LocatorAwareTrait;
 
     protected SlackClient $slackClient;
-    protected MessagesTable $Messages;
 
     public function __construct()
     {
         $this->slackClient = new SlackClient();
-        $this->Messages = $this->fetchTable('Messages');
     }
 
     public function gib(
@@ -30,7 +25,9 @@ class AwardService
         User $toUser,
         MessageEvent|ReactionAddedEvent $event,
     ) {
-        $message = $this->Messages->newEntity([
+        $messagesTable = $this->fetchTable('Messages');
+
+        $message = $messagesTable->newEntity([
             'sender_user_id' => $fromUser->id,
             'receiver_user_id' => $toUser->id,
             'amount' => $event->amount,
@@ -43,24 +40,14 @@ class AwardService
                 'type' => true,
             ],
         ]);
-        $this->Messages->saveOrFail($message);
+        $messagesTable->saveOrFail($message);
 
         if ($fromUser->notifications['sent'] === true) {
-            $query = $this->Messages->find();
-            $result = $query
-                ->select([
-                    'given_out' => $query->func()->sum('amount')
-                ])
-                ->where([
-                    'sender_user_id' => $toUser->id,
-                    'type' => Message::TYPE_POTATO,
-                    'created >=' => new FrozenTime('24 hours ago'),
-                ])
-                ->first();
+            $potatoLeftToday = $fromUser->potatoLeftToday();
 
             $gibMessage = sprintf('You did gib *%s* %s to <@%s>.', $event->amount, $event->reaction, $toUser->slack_user_id);
             $gibMessage .= PHP_EOL;
-            $gibMessage .= sprintf('You have *%s* :potato: left.', Message::MAX_AMOUNT - $result->given_out);
+            $gibMessage .= sprintf('You have *%s* :potato: left.', $potatoLeftToday);
             $gibMessage .= PHP_EOL;
             $gibMessage .= sprintf('> %s', $event->permalink);
 
@@ -75,7 +62,6 @@ class AwardService
             $receivedMessage .= PHP_EOL;
             $receivedMessage .= sprintf('> %s', $event->permalink);
 
-            // @FIXME Allow users to opt-out
             $this->slackClient->postMessage(
                 channel: $toUser->slack_user_id,
                 text: $receivedMessage,
