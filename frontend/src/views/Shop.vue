ecb04a5e1e46fec3a4fb6ebf519f2ade501680aa
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-03 01:19:24 +0100

    Style Shop page palce holder

--- frontend/src/views/Shop.vue
+++ frontend/src/views/Shop.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1>Shop</h1>
+    <div class="absolute left-0 bottom-0 right-0 top-0 flex items-center justify-center">
+        <h1 class="text-4xl font-semibold">Coming soon...</h1>
+    </div>
 </template>
