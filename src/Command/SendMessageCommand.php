d9357769407a3067eb0ecaf642310b11ecf68c73
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-03 03:39:14 +0100

    Add SendMessage command

---
+++ src/Command/SendMessageCommand.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Service\SlackClient;
+use Cake\Command\Command;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
+
+/**
+ * SendMessage command.
+ */
+class SendMessageCommand extends Command
+{
+    /**
+     * Hook method for defining this command's option parser.
+     *
+     * @see https://book.cakephp.org/4/en/console-commands/commands.html#defining-arguments-and-options
+     * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined
+     * @return \Cake\Console\ConsoleOptionParser The built parser.
+     */
+    public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser
+    {
+        $parser = parent::buildOptionParser($parser);
+
+        $parser->addArguments([
+            'channel' => ['help' => 'The channel to post the message to', 'required' => true],
+            'message' => ['help' => 'The message to be send', 'required' => true]
+        ]);
+
+        return $parser;
+    }
+
+    /**
+     * Implement this method with your command's logic.
+     *
+     * @param \Cake\Console\Arguments $args The command arguments.
+     * @param \Cake\Console\ConsoleIo $io The console io
+     * @return null|void|int The exit code or null for success
+     */
+    public function execute(Arguments $args, ConsoleIo $io)
+    {
+        $slackClient = new SlackClient();
+
+        $slackClient->postMessage(
+            channel: $args->getArgument('channel'),
+            text: $args->getArgument('message')
+        );
+    }
+}
