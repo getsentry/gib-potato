28de8445ee4792bf34b8972084ab5ce6c6bcc176
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-05 00:32:05 +0100

    ref(frontend): Remove js-cookie and use axiois defaults instead

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
-import Cookies from 'js-cookie'
 
 const api = {
     init() {
         axios.defaults.baseURL = '/api/'
-        axios.defaults.headers.common['Content-Type'] = 'application/json'
-        axios.defaults.headers.common['Accept'] = 'application/json'
-        axios.defaults.headers.common['X-CSRF-Token'] = Cookies.get('csrfToken')
+        axios.defaults.xsrfCookieName = 'csrfToken';
+        axios.defaults.xsrfHeaderName = 'X-Csrf-Token';
     },
 
     get(resource, id = '', options = {}) {
