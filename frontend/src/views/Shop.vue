359c427b783ea47e1366d190ee8e50744b18bf89
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-05 00:44:02 +0100

    ref(frontend): Allign shop placeholder design

--- frontend/src/views/Shop.vue
+++ frontend/src/views/Shop.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="absolute inset-0 flex items-center justify-center">
-        <h1 class="text-4xl font-semibold">Coming soon...</h1>
+        <h1 class="text-2xl font-extrabold">Coming soon...</h1>
     </div>
 </template>
