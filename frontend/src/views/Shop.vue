42fcef450a3a96679d4f63638cf4d5e5b336e7e7
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-05 00:36:08 +0100

    ref(frontend): Simplify Tailwind classes

--- frontend/src/views/Shop.vue
+++ frontend/src/views/Shop.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute left-0 bottom-0 right-0 top-0 flex items-center justify-center">
+    <div class="absolute inset-0 flex items-center justify-center">
         <h1 class="text-4xl font-semibold">Coming soon...</h1>
     </div>
 </template>
