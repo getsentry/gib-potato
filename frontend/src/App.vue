b51d18b5c0bf51f99b14b1b299418ae51f374a59
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-05 00:44:17 +0100

    ref(frontend): Rename Menu to MainMenu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-    <Menu />
+    <MainMenu />
     <div class="max-w-7xl mx-auto p-4 md:p-8">
         <RouterView />
     </div>
 </template>
 
 <script>
-import Menu from '@/components/Menu.vue';
+import MainMenu from '@/components/MainMenu.vue';
 
 export default {
     name: "App",
-    components: { Menu },
+    components: { MainMenu },
 };
 </script>
