5bcae90a7344eb292f1ae0b317c7cf8062b5c296
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-05 01:32:13 +0100

    fix(backend): Fix SendMessage command

--- src/Command/SendMessageCommand.php
+++ src/Command/SendMessageCommand.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Service\SlackClient;
+use App\Http\SlackClient;
 use Cake\Command\Command;
 use Cake\Console\Arguments;
 use Cake\Console\ConsoleIo;
