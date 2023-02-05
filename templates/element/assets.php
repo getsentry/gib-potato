b867a4313a78d130208a766e854db08269f8e906
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-05 19:00:02 +0100

    ref(backend): Use http for debug assets

--- templates/element/assets.php
+++ templates/element/assets.php
@@ -7,8 +7,8 @@ declare(strict_types=1);
 use Cake\Core\Configure;
 
 if (Configure::read('debug')) {
-    echo $this->Html->script('https://localhost:5173/@vite/client', ['type' => 'module']);
-    echo $this->Html->script('https://localhost:5173/frontend/src/main.js', ['type' => 'module']);
+    echo $this->Html->script('http://localhost:5173/@vite/client', ['type' => 'module']);
+    echo $this->Html->script('http://localhost:5173/frontend/src/main.js', ['type' => 'module']);
 } else {
     echo $this->Vite->css('frontend/src/main.js');
     echo $this->Vite->script('frontend/src/main.js');
