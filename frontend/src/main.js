0e67912ea4087504e6cf86d85b6dbb5125db4e52
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-08 00:26:11 +0100

    ref(frontend): Add config options to Sentry.init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,6 +16,8 @@ import './assets/main.css'
     Sentry.init({
         app,
         dsn: import.meta.env.VITE_APP_SENTRY_DSN,
+        environment: import.meta.env.VITE_APP_ENVIRONMENT,
+        release: import.meta.env.VITE_APP_VERSION,
         integrations: [
             new BrowserTracing({
                 routingInstrumentation: Sentry.vueRouterInstrumentation(router),
