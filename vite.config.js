3889edab6c2875325a28f305e93650bf961bc69e
Luca Forstner <luca.forstner@sentry.io>
2023-04-05 17:29:24 +0200

    meta(frontend): Enable `@sentry/vite-plugin` experiments

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+// @ts-check
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { sentryVitePlugin } from '@sentry/vite-plugin'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     sentryVitePlugin({
-      include: './webroot/assets',
+      include: [], // we're using debug ID upload instead (see _experiments.debugIdUpload)
       org: process.env.SENTRY_ORG,
       project: process.env.SENTRY_PROJECT,
       authToken: process.env.SENTRY_AUTH_TOKEN,
       dryRun: !process.env.SENTRY_AUTH_TOKEN,
-      cleanArtifacts: true,
-      stripCommonPrefix: true,
-      rewrite: true,
       setCommits: {
         auto: true,
         ignoreEmpty: true,
+      },
+      _experiments: {
+        injectBuildInformation: true,
+        debugIdUpload: {
+          include: './webroot/assets/**',
+        },
       },
     }),
   ],
@@ -30,13 +34,13 @@ export default defineConfig({
     manifest: true,
     minify: 'esbuild',
     rollupOptions: {
-      input: './frontend/src/main.js'
+      input: './frontend/src/main.js',
     },
   },
   envDir: './frontend/config',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./frontend/src', import.meta.url))
+      '@': fileURLToPath(new URL('./frontend/src', import.meta.url)),
     },
   },
-})
+});
