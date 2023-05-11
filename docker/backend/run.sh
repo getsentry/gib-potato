d61d4fddec9956a428bd1f5655b90a3cd563f16b
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-05-11 15:48:23 +0200

    Improve backend Docker setup

--- docker/backend/run.sh
+++ docker/backend/run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-service apache2 start
-service php8.1-fpm start
-
-bin/cake migrations migrate
-
-# Start supervisord and services
 exec /usr/bin/supervisord  -n -c /etc/supervisor/supervisord.conf
