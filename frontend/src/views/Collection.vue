c3a6d56feb84a68084328e3c6740c9bd83e436a4
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-09-05 22:39:54 +0200

    fix(frontend): Fix collection styling

--- frontend/src/views/Collection.vue
+++ frontend/src/views/Collection.vue
@@ -3,7 +3,10 @@
         v-if="collection.length"
         class="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 mb-32"
     >
-        <div v-for="(item, index) in collection">
+        <div
+            v-for="(item, index) in collection"
+            class="h-full flex flex-col"
+        >
             <div
                 :index="index"
                 class="relative"
@@ -14,15 +17,18 @@
                 <div class="relative mt-4">
                     <h3 class="text-sm font-medium">{{ item.name }}</h3>
                     <p class="mt-1 text-sm text-zinc-500">{{ item.description }}</p>
-                    <p class="mt-3 text-xs text-zinc-500">
-                        <template v-if="item.presentee_id === user.id">
-                            Received {{ new Date(item.created).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"}) }}
-                        </template>
-                        <template v-else>
-                            Purchased {{ new Date(item.created).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"}) }}
-                        </template>
-                    </p>
+
                 </div>
+            </div>
+            <div class="mt-auto">
+                <p class="mt-3 text-xs text-zinc-500">
+                    <template v-if="item.presentee_id === user.id">
+                        Received {{ new Date(item.created).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"}) }}
+                    </template>
+                    <template v-else>
+                        Purchased {{ new Date(item.created).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"}) }}
+                    </template>
+                </p>
             </div>
         </div>
     </div>
