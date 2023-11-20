ce5357f651d2925fcf1019697e04d0175b626b3c
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-11-21 00:32:38 +0100

    Revert "ref(backend): Enable SDK logging"
    This reverts commit 3cad477cf1b2d3e29fc1d6489c3b510b349e805a.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -42,8 +42,6 @@ use Cake\Mailer\Mailer;
 use Cake\Mailer\TransportFactory;
 use Cake\Routing\Router;
 use Cake\Utility\Security;
-use Sentry\Logger\DebugStdOutLogger;
-
 use function Cake\Core\env;
 use function Sentry\init;
 
@@ -108,7 +106,6 @@ init([
         ROOT . DS . 'vendor',
     ],
     'attach_stacktrace' => true,
-    'logger' => new DebugStdOutLogger(),
 ]);
 
 /*
