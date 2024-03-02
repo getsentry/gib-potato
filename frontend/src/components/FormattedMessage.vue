f103f668ba0b7562e84c336fd2e4175b8bc9abfa
Daniel Griesser <daniel.griesser.86@gmail.com>
2024-03-02 21:52:13 +0100

    Added formatted message

---
+++ frontend/src/components/FormattedMessage.vue
@@ -0,0 +1,18 @@
+<template>
+    <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white" v-html="formattedMessage">
+        
+  </p>
+  </template>
+  
+  <script>
+  export default {
+    props: ['message'],
+    computed: {
+        formattedMessage() {
+            return this.message
+                .replace(/<(@[^>]+)>/g, (_match, p1) => `<span class="bg-blue-700 p-0.5 px-1 rounded-lg">${p1}</span>`)
+                .replace(/:potato:/g, '🥔');
+        },  
+    },
+  }
+  </script>
--- frontend/src/views/Quickwins.vue
+++ frontend/src/views/Quickwins.vue
@@ -11,19 +11,19 @@
                 :index="index"
                 class="relative"
             >
-                <div class="flex rounded-full text-sm">
-                    <img class="h-8 w-8 rounded-full" :src="item.user.slack_picture">
+                <div class="flex items-start gap-2.5">
+                    <img class="w-8 h-8 rounded-full" :src="item.user.slack_picture" :alt="item.user.slack_name">
+                    <div class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                        <div class="flex items-center space-x-2 rtl:space-x-reverse">
+                            <span class="text-sm font-semibold text-gray-900 dark:text-white">{{item.user.slack_name}}</span>
+                            <span class="text-sm font-normal text-gray-500 dark:text-gray-400">{{ new Date(item.created).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"}) }}</span>
+                        </div>
+                        <FormattedMessage :message="item.message" />
+                        <span v-if="item.reaction_count >= 2" class="relative text-sm font-normal text-gray-500 dark:text-gray-400">
+                            <span v-for="(i, index2) in item.reaction_count" :key="i" :class="index2 === 0 ? '' : '-ml-2'">🥔</span>
+                        </span>
+                    </div>
                 </div>
-                <div class="relative mt-4">
-                    
-                    <p class="mt-1 text-sm text-zinc-500">{{ item.message }}</p>
-
-                </div>
-            </div>
-            <div class="mt-auto">
-                <p class="mt-3 text-xs text-zinc-500">
-                    Posted {{ new Date(item.created).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"}) }}
-                </p>
             </div>
         </div>
     </div>
@@ -40,9 +40,13 @@
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import FormattedMessage from '../components/FormattedMessage.vue';
 
 export default {
     name: 'Quickwins',
+    components: {
+        FormattedMessage,
+    },
     setup() {
         const store = useStore()
 
