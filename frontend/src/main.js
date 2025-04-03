f3cb3838cf6099b53e5277a4f0cb00b1c4d3e628
Michael Hoffmann <michi@sentry.io>
2025-04-03 15:50:02 +0200

    wip

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -64,7 +64,7 @@ import './assets/main.css'
                     boxShadow: 'none',
                 },
             }),
-            integrations: [Sentry.consoleLoggingIntegration({ levels: ['error'] })],
+            Sentry.consoleLoggingIntegration({ levels: ['error'] }),
         ],
     })
 
