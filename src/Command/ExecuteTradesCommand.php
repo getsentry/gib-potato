68e1122534ad237886a1e772fd895dd7d7fba1dd
Michael Hoffmann <michael.hoffmann@sentry.io>
2025-04-11 19:21:14 +0200

    Revert "fix(backend): Fix rounding (#289)"
    This reverts commit 5cbc35f5567916cce834702921e9a239eb3145ae.

--- src/Command/ExecuteTradesCommand.php
+++ src/Command/ExecuteTradesCommand.php
@@ -235,7 +235,7 @@ class ExecuteTradesCommand extends Command
             $sharePriceBuyAverage = $doneBuyTradesAverage[$stock->id]['average'] ?? $stock->share_prices[0]->price;
             $sharePriceSellAverage = $doneSellTradesAverage[$stock->id]['average'] ?? $stock->share_prices[0]->price;
 
-            $average = (int)round(($sharePriceBuyAverage + $sharePriceSellAverage) / 2, 0);
+            $average = ((int)round($sharePriceBuyAverage + $sharePriceSellAverage)) / 2;
 
             $newSharePrices[] = [
                 'stock_id' => $stock->id,
