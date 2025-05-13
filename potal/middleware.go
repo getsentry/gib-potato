93ead0ca1436f867eb34308a5fa6d5724c1128dd
Michael Hoffmann <michi@sentry.io>
2025-05-13 13:28:39 +0200

    chore(potal): Bump Go SDK

--- potal/middleware.go
+++ potal/middleware.go
@@ -6,17 +6,21 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/julienschmidt/httprouter"
 	"github.com/slack-go/slack"
 )
 
 func slackVerification(h httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		logger := sentry.NewLogger(r.Context())
+
 		// Verify the Slack request
 		// see https://github.com/slack-go/slack/blob/master/examples/workflow_step/middleware.go
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			logger.Error(r.Context(), "[slackVerification] Bad Request")
 			return
 		}
 		r.Body.Close()
@@ -25,16 +29,19 @@ func slackVerification(h httprouter.Handle) httprouter.Handle {
 		sv, err := slack.NewSecretsVerifier(r.Header, os.Getenv("SLACK_SIGNING_SECRET"))
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			logger.Error(r.Context(), "[slackVerification] Bad Request")
 			return
 		}
 
 		if _, err := sv.Write(body); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			logger.Error(r.Context(), "[slackVerification] Interal Server Error")
 			return
 		}
 
 		if err := sv.Ensure(); err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
+			logger.Error(r.Context(), "[slackVerification] Unauthorized")
 			return
 		}
 
