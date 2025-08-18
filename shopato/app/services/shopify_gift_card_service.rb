1a33df69dc11388288860fd71ac5cae65e087252
Johannes Daxböck <johannes.daxboeck@sentry.io>
2025-08-18 15:21:39 +0200

    Add service to call Shopify API to create gift card

---
+++ shopato/app/services/shopify_gift_card_service.rb
@@ -0,0 +1,57 @@
+class ShopifyGiftCardService
+  CREATE_MUTATION = <<~GRAPHQL
+    mutation giftCardCreate($input: GiftCardCreateInput!) {
+      giftCardCreate(input: $input) {
+        giftCard {
+          id
+          note
+          initialValue { amount }
+        }
+        giftCardCode
+        userErrors { message field code }
+      }
+    }
+  GRAPHQL
+
+  def initialize(shop: ENV.fetch("SHOPIFY_SHOP_DOMAIN"), token: ENV.fetch("SHOPIFY_ADMIN_ACCESS_TOKEN"))
+    @session = ShopifyAPI::Auth::Session.new(shop: shop, access_token: token)
+    @client  = ShopifyAPI::Clients::Graphql::Admin.new(session: @session)
+  end
+
+  def create_gift_card(amount, email = nil, note: nil)
+    variables = {
+      input: {
+        initialValue: format("%.2f", amount.to_f),
+        note: note.presence || "Issued via Gib🥔 for #{email}"
+      }
+    }
+
+    response = @client.query(query: CREATE_MUTATION, variables: variables)
+    payload  = response.body.dig("data", "giftCardCreate")
+
+    return error!("Unexpected response") if payload.nil?
+
+    errors = Array(payload["userErrors"]).map { |e| e["message"] }
+    return error!(errors.join(", ")) if errors.any?
+
+    success!(
+      gift_card: {
+        "id"     => payload.dig("giftCard", "id"),
+        "amount" => payload.dig("giftCard", "initialValue", "amount"),
+        "code"   => payload["giftCardCode"]
+      }
+    )
+  rescue => e
+    error!("Shopify error: #{e.message}")
+  end
+
+  private
+
+  def success!(gift_card:)
+    { success: true, gift_card: gift_card }
+  end
+
+  def error!(message)
+    { success: false, message: message, errors: [ message ] }
+  end
+end
---
+++ shopato/test/services/shopify_gift_card_service_test.rb
@@ -0,0 +1,55 @@
+require "test_helper"
+
+class ShopifyGiftCardServiceTest < ActiveSupport::TestCase
+  def setup
+    super
+    WebMock.disable_net_connect!
+  end
+
+  def teardown
+    WebMock.allow_net_connect!
+    super
+  end
+
+  def test_creates_gift_card_with_token_only_session
+    # Mock the GraphQL request to Shopify
+    stub_request(:post, "https://erdapfelshop.myshopify.com/admin/api/2025-07/graphql.json")
+      .with(
+        headers: {
+          "X-Shopify-Access-Token" => "1234_eräpfelkaas",
+          "Content-Type" => "application/json"
+        },
+        body: {
+          query: ShopifyGiftCardService::CREATE_MUTATION,
+          variables: {
+            input: {
+              initialValue: "25.00",
+              note: "Issued via Gib🥔 for mr.potato@eräpfel.com"
+            }
+          }
+        }.to_json
+      )
+      .to_return(
+        status: 200,
+        headers: { "Content-Type" => "application/json" },
+        body: {
+          data: {
+            giftCardCreate: {
+              giftCard: {
+                id: "gid://shopify/GiftCard/123",
+                initialValue: { amount: "25.00" }
+              },
+              giftCardCode: "POTATO-1234",
+              userErrors: []
+            }
+          }
+        }.to_json
+      )
+
+    service = ShopifyGiftCardService.new(shop: "erdapfelshop.myshopify.com", token: "1234_eräpfelkaas")
+    result = service.create_gift_card(25, "mr.potato@eräpfel.com")
+
+    assert result[:success]
+    assert_equal "POTATO-1234", result[:gift_card]["code"]
+  end
+end
