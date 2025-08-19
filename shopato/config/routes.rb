61b5ffbb0f0063547c77f2be24f389fb315f5edf
Johannes Daxböck <johannes.daxboeck@sentry.io>
2025-08-19 13:10:21 +0200

    Add tractor snake game for lulz 🚜 🥔

--- shopato/config/routes.rb
+++ shopato/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", :as => :rails_health_check
 
+  root "home#index"
+
   post "/gift-card", to: "gift_card#create"
 end
