78fca11ea8e9a5bbfae19e10042203c21ac471a7
Michael Hoffmann <michael.hoffmann@sentry.io>
2025-09-02 08:53:27 +0200

    chore(shopato): Add `config.profiles_sample_rate`

--- shopato/config/initializers/sentry.rb
+++ shopato/config/initializers/sentry.rb
@@ -10,6 +10,7 @@ Sentry.init do |config|
 
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.traces_sample_rate = 1.0
+  config.profiles_sample_rate = 1.0
   config.send_default_pii = true
 
   config.enable_logs = true
