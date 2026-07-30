b606a289a13b55df254b1ac7c19ef52a02acd876
Martin Linzmayer <martin.linzmayer@sentry.io>
2026-07-30 16:26:07 -0400

    restore client

---
+++ src/Http/Client.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http;
+
+use Cake\Http\Client as CakeClient;
+use Cake\Http\Client\Response;
+use Psr\Http\Message\RequestInterface;
+use Sentry\SentrySdk;
+use Sentry\Tracing\SpanContext;
+
+class Client extends CakeClient
+{
+    /**
+     * @inheritDoc
+     */
+    protected function _sendRequest(RequestInterface $request, array $options): Response
+    {
+        $parentSpan = SentrySdk::getCurrentHub()->getSpan();
+        $span = null;
+
+        if ($parentSpan !== null) {
+            $context = new SpanContext();
+            $context->setOp('http.client');
+            $context->setDescription(
+                sprintf(
+                    '%s %s://%s%s',
+                    strtoupper($request->getMethod()),
+                    $request->getUri()->getScheme(),
+                    $request->getUri()->getHost(),
+                    $request->getUri()->getPath(),
+                ),
+            );
+            $context->setData([
+                'http.query' => $request->getUri()->getQuery(),
+                'http.fragment' => $request->getUri()->getFragment(),
+            ]);
+            $span = $parentSpan->startChild($context);
+            SentrySdk::getCurrentHub()->setSpan($span);
+        }
+
+        $response = parent::_sendRequest($request, $options);
+
+        if ($span !== null) {
+            $span
+                ->setHttpStatus($response->getStatusCode())
+                ->setData([
+                    'http.response.status_code' => $response->getStatusCode(),
+                ])
+                ->finish();
+
+            if ($parentSpan !== null) {
+                SentrySdk::getCurrentHub()->setSpan($parentSpan);
+            }
+        }
+
+        return $response;
+    }
+}
