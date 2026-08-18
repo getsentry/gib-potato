b8cdb9214406044a3468a8efabaf5dc07ffe18d8
Michael Hoffmann <michi@sentry.io>
2026-08-18 15:51:55 +0200

    Ignore dieter when running vp fmt

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
         fmt: {
             singleQuote: true,
             tabWidth: 4,
+            ignorePatterns: ['dieter/**'],
         },
         plugins: [
             vue(),
